5390e15991412f5aae3256e3105742c738a5915f
Jcastle52 <castillojasiel11@gmail.com>
2025-07-11 00:43:42 -0500

    added the player detail component and delete button

--- index.js
+++ index.js
@@ -6,20 +6,18 @@ const $app = document.querySelector("#app");
 
 // State Variables
 let playerArr = [];
+let selectedPlayer;
 let teamArr = [];
-let SelectedPlayer;
 
 //-------------------------------------------------------------------------------------------------------------
 // API FUNCTIONS
 
-/* Gets all the players data stored in the API and updates the variable "playerArr"
-and then re-renders the page*/
+// Gets all the players data stored in the API and updates the variable "playerArr"
 const GetAllPlayers = async () => {
   try {
     const response = await fetch(API + "/players");
     const result = await response.json();
     playerArr = result.data.players;
-    Render();
   } catch (error) {
     console.error(error.message);
   }
@@ -41,6 +39,7 @@ const AddPlayer = async (name, breed, imageUrl) => {
       },
       body: JSON.stringify(newPlayer),
     });
+    await GetAllPlayers();
     Render();
   } catch (error) {
     console.error(error.message);
@@ -56,24 +55,27 @@ const RemovePlayer = async (id) => {
         "Content-Type": "application/json",
       },
     });
+    selectedPlayer = null;
+    await GetAllPlayers();
     Render();
   } catch (error) {
     console.error(error.message);
   }
 };
 
-// Gets player from the API given ID and returns the player data in an object, Used for Player Details section
+// Gets player object from the API given the ID and updates selectedPlayer variable and then re-renders page
 const GetPlayer = async (id) => {
   try {
     const response = await fetch(API + "/players/" + id);
     const result = await response.json();
-    return result.data.player;
+    selectedPlayer = result.data.player;
+    Render();
   } catch (error) {
     console.error(error.message);
   }
 };
 
-//Gets teams from API and updates the "teamArr" variable
+//Gets teams from API and updates the "teamArr" variable and returns a team name given the team id and player id
 const GetTeams = async () => {
   try {
     const response = await fetch(API + "/teams");
@@ -88,6 +90,59 @@ const GetTeams = async () => {
 //COMPONENT FUNCTIONS
 
 
+
+//Returns the a Team name given the team id, Its used it the player details component
+const GetTeam = (teamId) => {
+  for (let team of teamArr) {
+    if (team.id === teamId) return team.name;
+  }
+  return "Unassigned";
+};
+
+//This function returns the Player Details Elemtent
+const PlayerDetailsComponent = () => {
+  if (!selectedPlayer) {
+    const $p = document.createElement("p");
+    $p.textContent = "Select a player to learn more.";
+    return $p;
+  } else {
+    const $player = document.createElement("section");
+    $player.innerHTML = `
+        <img src=${selectedPlayer.imageUrl} alt=${selectedPlayer.name} width="200" height="300">
+        <h3>Name: ${selectedPlayer.name}</h3>
+        <h3>ID: ${selectedPlayer.id}</h3>
+        <h3>Status: ${selectedPlayer.status}</h3>
+        <h3>Breed: ${selectedPlayer.breed}</h3>
+        <h3>Team: ${GetTeam(selectedPlayer.teamId)}</h3>
+        <button type="button" id="delete">Remove From Roster</button>
+        `;
+
+    const $delete = $player.querySelector("#delete");
+    $delete.addEventListener("click", () => RemovePlayer(selectedPlayer.id));
+
+    return $player;
+  }
+};
+
+//This function returns the Player List Element
+const PlayerListComponent = () => {
+  const $list = document.createElement("ul");
+
+  for (let player of playerArr) {
+    const $player = document.createElement("li");
+    $player.innerHTML = `
+        <a href="#selected">
+        <img src=${player.imageUrl} alt=${player.name} width="100" height="150">
+        ${player.name}
+        </a>
+        `;
+    $list.append($player);
+
+    //This eventlistener updates the SelectedPlayer variable and then re-renders the page
+    $player.addEventListener("click", () => GetPlayer(player.id));
+  }
+  return $list;
+};
 
 //Gets called after a state variable has been changed. Also Renders the initial page elements
 const Render = () => {
@@ -109,9 +164,15 @@ const Render = () => {
     </main>
     `;
 
-    $app.querySelector("PlayersList").replaceWith();
-    $app.querySelector("SelectedPlayer").replaceWith();
-    $app.querySelector("InvitePuppy").replaceWith();
+  $app.querySelector("PlayersList").replaceWith(PlayerListComponent());
+  $app.querySelector("SelectedPlayer").replaceWith(PlayerDetailsComponent());
+  $app.querySelector("InvitePuppy").replaceWith();
 };
 
-Render();
+const Run = async () => {
+  await GetAllPlayers();
+  await GetTeams();
+  Render();
+};
+
+Run();
